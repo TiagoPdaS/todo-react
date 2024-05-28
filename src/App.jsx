9bdc71e32189ec7affe71220984a68bf3b58184a
myuser <myemail>
2024-05-28 19:58:12 -0400

    1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,47 @@
 import { useState } from "react";
+import Todo from "./components/Todo";
 import "./App.css";
-
+import TodoForm from "./components/TodoForm";
 
 function App() {
-    const [todos, setTodos] = useState([
-        {
-            id: 1,
-            text:"EStudar React",
-            category: "Trabalho",
-            isCompleted:"false",
-        },
-        {
-            id: 2,
-            text:"Ir para a Academia",
-            category: "Pessoal",
-            isCompleted:"false",
-        },
-        {
-            id: 3,
-            text:"Criar app To-Do",
-            category: "Trabalho",
-            isCompleted:"false",
-        },
-        {
-            id: 4,
-            text:"EStudar Ingles",
-            category: "Pessoal",
-            isCompleted:"false",
-        },
-    ])
- return <div className="app">
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "EStudar React",
+      category: "Trabalho",
+      isCompleted: "false",
+    },
+    {
+      id: 2,
+      text: "Ir para a Academia",
+      category: "Pessoal",
+      isCompleted: "false",
+    },
+    {
+      id: 3,
+      text: "Criar app To-Do",
+      category: "Trabalho",
+      isCompleted: "false",
+    },
+    {
+      id: 4,
+      text: "EStudar Ingles",
+      category: "Pessoal",
+      isCompleted: "false",
+    },
+  ]);
 
-<h1>Lista de Tarefas</h1>
-<div className="todo-list">
-    {todos.map((todo) => (
-        <div className="todo">
-            <div className="constent">
-                <p>{todo.text}</p>
-                <p className="category"> ({todo.category})</p>
-            </div>
-
-            <div>
-                <button>Completar</button>
-                <button>X</button>
-            </div>
-
-        </div>
-    ))}
-</div>
-
- </div>;
+  return (
+    <div className="app">
+      <h1>Lista de Tarefas</h1>
+      <div className="todo-list">
+        {todos.map((todo) => (
+          <Todo todo={todo} />
+        ))}
+      </div>
+      <TodoForm />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/TodoForm.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function TodoForm(){
+  return (
+    <div className='todo-form'> 
+      <h2>Criar Tarefa:</h2>
+    </div>
+  );
+}
+
+export default TodoForm;
