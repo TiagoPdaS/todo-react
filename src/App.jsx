a37cc9ca1c833c5e336e216fb9795fa32ec44abc
myuser <myemail>
2024-05-28 19:16:20 -0400

    1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,55 @@
 import { useState } from "react";
+import "./App.css";
 
 
 function App() {
- return <div>Ola Mundo!</div>
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            text:"EStudar React",
+            category: "Trabalho",
+            isCompleted:"false",
+        },
+        {
+            id: 2,
+            text:"Ir para a Academia",
+            category: "Pessoal",
+            isCompleted:"false",
+        },
+        {
+            id: 3,
+            text:"Criar app To-Do",
+            category: "Trabalho",
+            isCompleted:"false",
+        },
+        {
+            id: 4,
+            text:"EStudar Ingles",
+            category: "Pessoal",
+            isCompleted:"false",
+        },
+    ])
+ return <div className="app">
+
+<h1>Lista de Tarefas</h1>
+<div className="todo-list">
+    {todos.map((todo) => (
+        <div className="todo">
+            <div className="constent">
+                <p>{todo.text}</p>
+                <p className="category"> ({todo.category})</p>
+            </div>
+
+            <div>
+                <button>Completar</button>
+                <button>X</button>
+            </div>
+
+        </div>
+    ))}
+</div>
+
+ </div>;
 }
 
 export default App;
