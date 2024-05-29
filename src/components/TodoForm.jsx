3e8bba186513400b367fef11764149348b0347ed
myuser <myemail>
2024-05-28 20:32:08 -0400

    front done

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -4,6 +4,16 @@ function TodoForm(){
   return (
     <div className='todo-form'> 
       <h2>Criar Tarefa:</h2>
+      <form>
+        <input type="text" placeholder="Digite o texto"/>
+        <select>
+          <option value="">Selecione uma Categoria</option>
+          <option value="Trabalho">Trabalho</option>
+          <option value="Pessoal">Pessoal</option>
+          <option value="Estudos">Estudos</option>
+        </select>
+        <button type="submit">Criar Tarefa</button>
+      </form>
     </div>
   );
 }
